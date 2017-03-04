0a6b3181beb9b01ec7608a45673e60e9f8dfc598
v4hn <me@v4hn.de>
2017-03-04 17:06:05 +0100

    plan_pick: reduce number of possible approaches

--- src/plan_pick.cpp
+++ src/plan_pick.cpp
@@ -54,6 +54,7 @@ int main(int argc, char** argv){
 		gengrasp->setGripperGraspPose("open");
 		gengrasp->setObject("object");
 		gengrasp->setGraspOffset(.03);
+		gengrasp->setAngleDelta(.2);
 		t.addAfter(gengrasp);
 	}
 
