0d338a7f5f96310b0474533c68a7df9412f74c65
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2018-02-06 09:02:35 +0100

    visualize collisions

--- core/src/stages/fix_collision_objects.cpp
+++ core/src/stages/fix_collision_objects.cpp
@@ -1,71 +1,132 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2017, Bielefeld University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Bielefeld University nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Authors: Elham Iravani, Robert Haschke
+   Desc:    Fix collisions in input scene
+*/
+
 #include <moveit/task_constructor/stages/fix_collision_objects.h>
 
 #include <moveit/task_constructor/storage.h>
 #include <moveit/planning_scene/planning_scene.h>
-#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <rviz_marker_tools/marker_creation.h>
+#include <Eigen/Geometry>
+#include <eigen_conversions/eigen_msg.h>
+#include <ros/console.h>
+
+namespace vm = visualization_msgs;
 
 namespace moveit { namespace task_constructor { namespace stages {
 
-FixCollisionObjects::FixCollisionObjects(std::string name)
+FixCollisionObjects::FixCollisionObjects(const std::string &name)
   : PropagatingEitherWay(name)
-{}
+{
+	auto& p = properties();
+	p.declare<double>("max_penetration", "maximally corrected penetration depth");
+}
 
-bool FixCollisionObjects::computeForward(const InterfaceState &from){
-	planning_scene::PlanningScenePtr to = apply(from.scene(), false);
-	sendForward(from, InterfaceState(to), robot_trajectory::RobotTrajectoryPtr());
-	return true;
+void FixCollisionObjects::setMaxPenetration(double penetration)
+{
+	setProperty("max_penetration", penetration);
+}
+
+bool FixCollisionObjects::computeForward(const InterfaceState &from)
+{
+	planning_scene::PlanningScenePtr to = from.scene()->diff();
+	SubTrajectory solution;
+	bool success = fixCollisions(*to, solution.markers());
+	if (!success) solution.setCost(std::numeric_limits<double>::infinity());
+	sendForward(from, InterfaceState(to), std::move(solution));
+	return success;
 }
 
 bool FixCollisionObjects::computeBackward(const InterfaceState &to)
 {
-	planning_scene::PlanningScenePtr from = apply(to.scene(), true);
-	sendBackward(InterfaceState(from), to, robot_trajectory::RobotTrajectoryPtr());
-	return true;
+	planning_scene::PlanningScenePtr from = to.scene()->diff();
+	SubTrajectory solution;
+	bool success = fixCollisions(*from, solution.markers());
+	if (!success) solution.setCost(std::numeric_limits<double>::infinity());
+	sendBackward(InterfaceState(from), to, std::move(solution));
+	return success;
 }
 
-void FixCollisionObjects::setDistanceThreshold(double penetration)
+bool FixCollisionObjects::fixCollisions(planning_scene::PlanningScene &scene, std::deque<visualization_msgs::Marker> &markers) const
 {
-	penetration_ = penetration;
-}
+	const auto& props = properties();
+	double penetration = props.get<double>("max_penetration");
 
-void FixCollisionObjects::checkCollisions(planning_scene::PlanningScene &scene, const std::string& object)
-{
-	collision_detection::AllowedCollisionMatrix acm = scene.getAllowedCollisionMatrix();
+	collision_detection::CollisionRequest req;
+	collision_detection::CollisionResult res;
+	req.group_name = "";  // check collisions for complete robot
+	req.contacts = true;
+	req.max_contacts = 100;
+	req.max_contacts_per_pair = 5;
+	req.verbose = false;
+	req.distance = false;
 
-	collision_detection::CollisionRequest collision_request;
-	collision_detection::CollisionResult collision_result;
-	collision_request.group_name = object;
-	collision_request.contacts = true;
-	collision_request.max_contacts = 100;
-	collision_request.max_contacts_per_pair = 5;
-	collision_request.verbose = false;
-	collision_request.distance = true;
-
-	scene.checkCollision(collision_request, collision_result, scene.getCurrentState());
+	scene.getCollisionWorld()->checkRobotCollision(req, res, *scene.getCollisionRobotUnpadded(), scene.getCurrentState(),
+	                                               scene.getAllowedCollisionMatrix());
 
 	geometry_msgs::Pose pose;
 
-	if(collision_result.collision)
-	{
-		std::cout<<"COLLIDING"<<std::endl;
-		if(collision_result.contacts.begin()->second.begin()->depth < penetration_)
-		{
-			pose.position.x = 0.3 + (collision_result.contacts.begin()->second.begin()->depth * collision_result.contacts.begin()->second.begin()->normal[0]);
-			pose.position.y = 0.23 + (collision_result.contacts.begin()->second.begin()->depth * collision_result.contacts.begin()->second.begin()->normal[2]);
-			pose.position.z = -0.02 + (collision_result.contacts.begin()->second.begin()->depth * collision_result.contacts.begin()->second.begin()->normal[1]);
+	if (!res.collision)
+		return true;
 
-			//correct collision
-			moveCollisionObject(scene, pose, object, "world");
+	vm::Marker m;
+	m.header.frame_id = scene.getPlanningFrame();
+	m.ns = "collisions";
+	rviz_marker_tools::setColor(m.color, rviz_marker_tools::RED);
+
+	ROS_INFO_THROTTLE(1, "collision(s) detected");
+	for (const auto& info : res.contacts) {
+		for (const auto& contact : info.second) {
+			tf::poseEigenToMsg(Eigen::Translation3d(contact.pos) * Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitX(), contact.normal), m.pose);
+			rviz_marker_tools::makeArrow(m, contact.depth);
+			markers.push_back(m);
 		}
 	}
-	else
-		std::cout<<"No Collision!!!!!!!!!!!!!"<<std::endl;
+
+	// check again
+	scene.getCollisionWorld()->checkRobotCollision(req, res, *scene.getCollisionRobotUnpadded(), scene.getCurrentState(),
+	                                               scene.getAllowedCollisionMatrix());
+	return !res.collision;
 }
 
-void FixCollisionObjects::moveCollisionObject(planning_scene::PlanningScene &scene, geometry_msgs::Pose pose, const std::string& object, const std::string& base_frame)
+void FixCollisionObjects::fixCollision(planning_scene::PlanningScene &scene, geometry_msgs::Pose pose, const std::string& object) const
 {
 	moveit_msgs::CollisionObject collision_obj;
-	collision_obj.header.frame_id = base_frame;
+	collision_obj.header.frame_id = scene.getPlanningFrame();
 	collision_obj.id = object;
 	collision_obj.operation = moveit_msgs::CollisionObject::MOVE;
 
@@ -74,16 +135,6 @@ void FixCollisionObjects::moveCollisionObject(planning_scene::PlanningScene &sce
 
 	if(!scene.processCollisionObjectMsg(collision_obj))
 		std::cout<<"Moving FAILED"<<std::endl;
-}
-
-planning_scene::PlanningScenePtr FixCollisionObjects::apply(const planning_scene::PlanningSceneConstPtr &scene, bool invert) const
-{
-	planning_scene::PlanningScenePtr result = scene->diff();
-
-	//Check collision of objects on the table in planning scene
-	(const_cast<FixCollisionObjects*>(this))->checkCollisions(*result, "object");
-
-	return result;
 }
 
 } } }
