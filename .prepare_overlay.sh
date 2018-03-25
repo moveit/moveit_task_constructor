01a2415149ee2e1440030d6aa4041f3d45278ff7
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2018-03-25 05:53:17 +0200

    fix travis: build not yet released dependencies

---
+++ .prepare_overlay.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# we need an unreleased version of moveit_core
+mkdir moveit
+cd moveit
+git init
+git remote add -f origin https://github.com/ros-planning/moveit.git
+git config core.sparseCheckout true
+echo "moveit_core" >> .git/info/sparse-checkout
+git pull origin ${ROS_DISTRO}_devel
