c3f42b66f708216b435cd8a9abd0dfe52efc0fb4
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2018-04-12 12:22:50 +0200

    travis fix

--- .prepare_overlay.sh
+++ .prepare_overlay.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # we need an unreleased version of moveit_core
 mkdir moveit
-cd moveit
+pushd moveit
 git init
 git remote add -f origin https://github.com/ros-planning/moveit.git
 git config core.sparseCheckout true
 echo "moveit_core" >> .git/info/sparse-checkout
-git pull origin ${ROS_DISTRO}_devel
+git pull origin ${ROS_DISTRO}-devel
+popd
