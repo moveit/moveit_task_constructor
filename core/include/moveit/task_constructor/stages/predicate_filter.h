cc90d5f3f295462f833d6cb93e1998e5db383dc0
v4hn <me@v4hn.de>
2018-09-23 16:58:10 +0200

    add PredicateFilter

---
+++ core/include/moveit/task_constructor/stages/predicate_filter.h
@@ -0,0 +1,69 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2018, Hamburg University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Bielefeld University nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+/* Authors: Michael Goerner */
+
+#pragma once
+
+#include <moveit/task_constructor/container.h>
+
+namespace moveit {
+namespace core {
+MOVEIT_CLASS_FORWARD(RobotState)
+}
+}
+
+namespace moveit { namespace task_constructor { namespace stages {
+
+/** Generator Wrapper to filter generated solutions by custom criteria
+ *
+ * All solutions of the wrapped class are passed to predicate.
+ * Solutions are accepted if predicate(s) == true.
+ * Rejected solutions are forwarded as failures with an optional comment
+ */
+class PredicateFilter : public WrapperBase {
+public:
+	typedef std::function<bool(const SolutionBase&, std::string&)> Predicate;
+
+	PredicateFilter(const std::string &name, Stage::pointer &&child = Stage::pointer());
+
+	void init(const moveit::core::RobotModelConstPtr& robot_model) override;
+
+	void onNewSolution(const SolutionBase &s) override;
+
+	void setPredicate(const Predicate& p){
+		setProperty("predicate", p);
+	}
+};
+
+} } }
