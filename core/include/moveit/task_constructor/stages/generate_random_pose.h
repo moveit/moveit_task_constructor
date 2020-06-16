a28bb0ab10f924dd9f3967210a5e6492cbc75a65
Henning Kayser <henningkayser@picknik.ai>
2020-06-16 16:35:06 +0200

    Support PoseDimension sampling order in GenerateRandomPose

--- core/include/moveit/task_constructor/stages/generate_random_pose.h
+++ core/include/moveit/task_constructor/stages/generate_random_pose.h
@@ -1,5 +1,39 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020, PickNik Inc
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PickNik Inc nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
 /* Authors: Henning Kayser
-   Desc:    Generator Stage for random poses based on GeneratePose
+   Desc:    Generator Stage for randomized poses based on GeneratePose
 */
 
 #pragma once
@@ -39,7 +73,9 @@ public:
 	 * Configure a RandomNumberDistribution (see https://en.cppreference.com/w/cpp/numeric/random) sampler for a
 	 * PoseDimension (X/Y/Z/ROLL/PITCH/YAW) for randomizing the pose.
 	 * The distribution_param is applied to the specified distribution method while the target pose value is used as
-	 * seed.
+	 * seed. The order in which the PoseDimension samplers are specified matters as the samplers are applied in sequence.
+	* That way it's possible to implement different Euler angles (i.e. XYZ, ZXZ, YXY) or even construct more complex
+	* sampling regions by applying translations after rotations.
 	 * Currently supported distributions are:
 	 * * std::uniform_real_distrubtion: distribution_param specifies the full value range around the target_pose value
 	 * * std::normal_distribution: distribution_param is used as stddev, target_pose value is mean
@@ -55,7 +91,7 @@ public:
 	 * Specify a sampling function of type PoseDimensionSampler for randomizing a pose dimension.
 	 */
 	void sampleDimension(const PoseDimension pose_dimension, const PoseDimensionSampler& pose_dimension_sampler) {
-		pose_dimension_samplers_[pose_dimension] = pose_dimension_sampler;
+		pose_dimension_samplers_.emplace_back(std::make_pair(pose_dimension, pose_dimension_sampler));
 	}
 
 	/*
@@ -71,7 +107,7 @@ private:
 		throw 0;  // suppress -Wreturn-type
 	}
 
-	std::map<PoseDimension, PoseDimensionSampler> pose_dimension_samplers_;
+	std::vector<std::pair<PoseDimension, PoseDimensionSampler>> pose_dimension_samplers_;
 };
 template <>
 GenerateRandomPose::PoseDimensionSampler
--- core/src/stages/generate_random_pose.cpp
+++ core/src/stages/generate_random_pose.cpp
@@ -1,3 +1,37 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020, PickNik Inc
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of PickNik Inc nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
 /* Authors: Henning Kayser */
 
 #include <moveit/task_constructor/stages/generate_random_pose.h>
@@ -6,9 +40,8 @@
 #include <moveit/planning_scene/planning_scene.h>
 #include <rviz_marker_tools/marker_creation.h>
 
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2/convert.h>
+#include <Eigen/Geometry>
+#include <eigen_conversions/eigen_msg.h>
 
 #include <chrono>
 
@@ -85,40 +118,38 @@ void GenerateRandomPose::compute() {
 
 	// Use target pose as seed pose
 	geometry_msgs::PoseStamped seed_pose = target_pose;
-	tf2::Quaternion seed_q, rand_q;
-	tf2::fromMsg(seed_pose.pose.orientation, seed_q);
+	Eigen::Isometry3d seed, sample;
+	tf::poseMsgToEigen(seed_pose.pose, seed);
 	double elapsed_time = 0.0;
 	const auto start_time = std::chrono::steady_clock::now();
 	size_t spawned_solutions = 0;
 	const size_t max_solutions = properties().get<size_t>("max_solutions");
 	while (elapsed_time < timeout() && ++spawned_solutions < max_solutions) {
-		// Randomize pose using specified dimension samplers.
-		// RPY dimensions are using 0.0 as seed, the randomized rotation is multiplied to the target orientation.
-		double rand_roll = 0.0, rand_pitch = 0.0, rand_yaw = 0.0;
+		// Randomize pose using specified dimension samplers applied in the order
+		// in which they have been specified
+		sample = seed;
 		for (const auto& pose_dim_sampler : pose_dimension_samplers_) {
 			switch (pose_dim_sampler.first) {
 				case X:
-					target_pose.pose.position.x = pose_dim_sampler.second(seed_pose.pose.position.x);
+					sample.translate(Eigen::Vector3d(pose_dim_sampler.second(seed_pose.pose.position.x), 0, 0));
 					break;
 				case Y:
-					target_pose.pose.position.y = pose_dim_sampler.second(seed_pose.pose.position.y);
+					sample.translate(Eigen::Vector3d(0, pose_dim_sampler.second(seed_pose.pose.position.y), 0));
 					break;
 				case Z:
-					target_pose.pose.position.z = pose_dim_sampler.second(seed_pose.pose.position.z);
+					sample.translate(Eigen::Vector3d(0, 0, pose_dim_sampler.second(seed_pose.pose.position.z)));
 					break;
 				case ROLL:
-					rand_roll = pose_dim_sampler.second(rand_roll);
+					sample.rotate(Eigen::AngleAxisd(pose_dim_sampler.second(0.0), Eigen::Vector3d::UnitX()));
 					break;
 				case PITCH:
-					rand_pitch = pose_dim_sampler.second(rand_pitch);
+					sample.rotate(Eigen::AngleAxisd(pose_dim_sampler.second(0.0), Eigen::Vector3d::UnitY()));
 					break;
 				case YAW:
-					rand_yaw = pose_dim_sampler.second(rand_yaw);
+					sample.rotate(Eigen::AngleAxisd(pose_dim_sampler.second(0.0), Eigen::Vector3d::UnitZ()));
 			}
 		}
-		rand_q.setRPY(rand_roll, rand_pitch, rand_yaw);
-		rand_q = seed_q * rand_q;
-		target_pose.pose.orientation = tf2::toMsg(rand_q);
+		tf::poseEigenToMsg(sample, target_pose.pose);
 
 		// Spawn sampled pose
 		spawn_target_pose(target_pose);
