cf0e4e3b017daafd8a3c8a944438b5e43566f12e
v4hn <me@v4hn.de>
2020-06-30 11:10:33 +0200

    establish struct cost pattern
    for costs with multiple parameters, this option-style is more useful
    than verbose getter/setter. There is nothing to hide here.

--- core/include/moveit/task_constructor/cost_terms.h
+++ core/include/moveit/task_constructor/cost_terms.h
@@ -8,15 +8,14 @@ namespace cost {
 
 /// These structures all implement the Stage::CostTerm API and can be configured via Stage::setCostTerm()
 /// add a constant cost to each solution
-class ConstantCost
+struct ConstantCost
 {
 public:
-	ConstantCost(double cost) : cost_(cost) {}
+	ConstantCost(double c) : cost(c) {}
 
-	double operator()(const SubTrajectory&) { return cost_; }
+	double operator()(const SubTrajectory&, std::string& /* unused */) const { return cost; }
 
-private:
-	double cost_;
+	double cost;
 };
 
 /// execution duration of the whole trajectory
