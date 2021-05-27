949024fd80470166e0d089ab6aa86ef09aee83bd
v4hn <me@v4hn.de>
2021-05-27 14:20:42 +0200

    split ClassLoader plugin code from main libraries
    to work around https://github.com/ros/class_loader/issues/131
    and have all code using the `CurrentState` stage sanitize correctly
    (unless they use it through ClassLoader).

--- core/src/stages/current_state.cpp
+++ core/src/stages/current_state.cpp
@@ -96,7 +96,3 @@ void CurrentState::compute() {
 }  // namespace stages
 }  // namespace task_constructor
 }  // namespace moveit
-
-/// register plugin
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(moveit::task_constructor::stages::CurrentState, moveit::task_constructor::Stage)
