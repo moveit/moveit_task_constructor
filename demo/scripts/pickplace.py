0dffefd6ae68def70257144b91966a430e9b40e1
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2022-01-17 10:56:44 +0100

    Cleanup pickplace.py

--- demo/scripts/pickplace.py
+++ demo/scripts/pickplace.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from moveit.python_tools import roscpp_init
 from moveit.task_constructor import core, stages
 from moveit_commander import PlanningSceneInterface
 from geometry_msgs.msg import PoseStamped, TwistStamped
-import rospy
 import time
 
-rospy.init_node("PickPlacePython")
+roscpp_init("pickplace")
 
 # Specify robot parameters
 arm = "panda_arm"
@@ -18,13 +18,10 @@ object_name = "grasp_object"
 object_radius = 0.02
 
 # Start with a clear planning scene
-psi = PlanningSceneInterface()
-time.sleep(0.2)
+psi = PlanningSceneInterface(synchronous=True)
 psi.remove_world_object()
-time.sleep(0.2)
 
 # Grasp object properties
-objectRadius = 1.0
 objectPose = PoseStamped()
 objectPose.header.frame_id = "world"
 objectPose.pose.orientation.x = 1.0
@@ -33,49 +30,40 @@ objectPose.pose.position.y = 0.0
 objectPose.pose.position.z = 0.285
 
 # Add the grasp object to the planning scene
-time.sleep(0.2)
-psi.add_sphere(object_name, objectPose, object_radius)
-time.sleep(0.2)
+psi.add_box(object_name, objectPose, size=[0.1, 0.05, 0.03])
 
 # Create a task
 task = core.Task("PandaPickPipelineExample")
 task.enableIntrospection()
 
 # Start with the current state
-current = stages.CurrentState("current")
-task.add(current)
+task.add(stages.CurrentState("current"))
 
 # Create a planner instance that is used to connect
 # the current state to the grasp approach pose
-pipelinePlanner = core.PipelinePlanner()
-pipelinePlanner.planner = "RRTConnectkConfigDefault"
-planners = [(arm, pipelinePlanner)]
+pipeline = core.PipelinePlanner()
+pipeline.planner = "RRTConnectkConfigDefault"
+planners = [(arm, pipeline)]
 
 # Connect the two stages
-connect = stages.Connect("connect1", planners)
-connect.properties.configureInitFrom(core.Stage.PropertyInitializerSource.PARENT)
-task.add(connect)
+task.add(stages.Connect("connect1", planners))
 
-# The grasp generator spawns a set of possible grasp
-# poses around the object
+# The grasp generator spawns a set of possible grasp poses around the object
 grasp_generator = stages.GenerateGraspPose("Generate Grasp Pose")
 grasp_generator.angle_delta = 0.2
 grasp_generator.pregrasp = "open"
 grasp_generator.grasp = "close"
-grasp_generator.setMonitoredStage(task["current"])
+grasp_generator.setMonitoredStage(task["current"])  # Generate solutions for all initial states
 
-# Make sure the object is not getting placed
-# in-collision with the base frame of the panda's gripper
-ikFrame = PoseStamped()
-ikFrame.header.frame_id = "panda_hand"
-ikFrame.pose.position.z = 0.09
+# SimpleGrasp container encapsulates IK calculation of arm pose as well as finger closing
+simpleGrasp = stages.SimpleGrasp(grasp_generator, "Grasp")
+# Set frame for IK calculation in the center between the fingers
+ik_frame = PoseStamped()
+ik_frame.header.frame_id = "panda_hand"
+ik_frame.pose.position.z = 0.1034
+simpleGrasp.setIKFrame(ik_frame)
 
-# Generate a simple grasp for every grasp generator pose.
-# This stage wraps the inverse kinematic computation
-simpleGrasp = stages.SimpleGrasp(grasp_generator, "Simple Grasp")
-simpleGrasp.setIKFrame(ikFrame)
-
-# Pick pipeline wrapper
+# Pick container comprises approaching, grasping (using SimpleGrasp stage), and lifting of object
 pick = stages.Pick(simpleGrasp, "Pick")
 pick.eef = eef
 pick.object = object_name
@@ -95,37 +83,28 @@ pick.setLiftMotion(lift, 0.03, 0.1)
 # Add the pick stage to the task's stage hierarchy
 task.add(pick)
 
-# Connect the pick stage with the now following
-# place stage
-connect = stages.Connect("connect2", planners)
-connect.properties.configureInitFrom(core.Stage.PropertyInitializerSource.PARENT)
-task.add(connect)
+# Connect the Pick stage with the following Place stage
+task.add(stages.Connect("connect2", planners))
 
-# Define the pose that the object should obtain
-# when it is placed
-placePose = PoseStamped()
-placePose.header.frame_id = "world"
-placePose.pose = objectPose.pose
-placePose.pose.position.y += 0.2  # modify place pose relative to pick pose
+# Define the pose that the object should have after placing
+placePose = objectPose
+placePose.pose.position.y += 0.2  # shift object by 20cm along y axis
 
-# The grasp generator samples possible grasp
-# frames around the destination pose.
-grasp_generator = stages.GeneratePlacePose("Generate Place Pose")
-grasp_generator.setMonitoredStage(task["Pick"])
-grasp_generator.object = object_name
-grasp_generator.pose = placePose
-grasp_generator.properties.configureInitFrom(
+# Generate Cartesian place poses for the object
+place_generator = stages.GeneratePlacePose("Generate Place Pose")
+place_generator.setMonitoredStage(task["Pick"])
+place_generator.object = object_name
+place_generator.pose = placePose
+place_generator.properties.configureInitFrom(
     core.Stage.PropertyInitializerSource.PARENT, ["ik_frame"]
 )
 
-# Generate a grasp for every sampled pose by again wrapping
-# inverse kinematics computation
-simpleUnGrasp = stages.SimpleUnGrasp(grasp_generator, "Simple Grasp")
-simpleUnGrasp.setIKFrame(ikFrame)
-simpleUnGrasp.grasp = "close"
+# The SimpleUnGrasp container encapsulates releasing the object at the given Cartesian pose
+simpleUnGrasp = stages.SimpleUnGrasp(place_generator, "UnGrasp")
+simpleUnGrasp.setIKFrame(ik_frame)
 simpleUnGrasp.pregrasp = "open"
 
-# Place wrapper stage
+# Place container comprises placing, ungrasping, and retracting
 place = stages.Place(simpleUnGrasp, "Place")
 place.eef = eef
 place.object = object_name
@@ -148,9 +127,10 @@ task.add(place)
 
 if task.plan():
     task.publish(task.solutions[0])
-time.sleep(1)
 
-# Prevent the program from exiting,
-# giving you the opportunity to inspect the solutions
-# visually via the solution topic in rviz.
-rospy.spin()
+# avoid ClassLoader warning
+del pipeline
+del planners
+
+# Prevent the program from exiting, giving you the opportunity to inspect solutions in rviz
+time.sleep(3600)
