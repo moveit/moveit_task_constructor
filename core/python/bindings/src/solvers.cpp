6ffc5082256aed9e2323818755ee08cb4f2f4164
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2022-01-23 22:35:56 +0100

    Finetune solvers documentation

--- core/python/bindings/src/solvers.cpp
+++ core/python/bindings/src/solvers.cpp
@@ -51,47 +51,29 @@ namespace moveit {
 namespace python {
 
 void export_solvers(py::module& m) {
-	properties::class_<PlannerInterface>(m, "PlannerInterface", "Base class for planning algorithms")
+	properties::class_<PlannerInterface>(m, "PlannerInterface", "Abstract base class for planning algorithms")
 	    .property<double>("max_velocity_scaling_factor", "float: Reduce the maximum velocity by scaling between (0,1]")
 	    .property<double>("max_acceleration_scaling_factor",
 	                      "float: Reduce the maximum acceleration by scaling between (0,1]")
 	    .def_property_readonly("properties", py::overload_cast<>(&PlannerInterface::properties),
 	                           py::return_value_policy::reference_internal, "Properties of the planner");
 
-	properties::class_<PipelinePlanner, PlannerInterface>(m, "PipelinePlanner", R"(
-			Use MoveIt's PlanningPipeline to plan a trajectory between to scenes.
-
+	properties::class_<PipelinePlanner, PlannerInterface>(m, "PipelinePlanner",
+	                                                      R"(Plan using MoveIt's ``PlanningPipeline``
 			::
 
-				# create and configure a planner instance
+				from moveit.task_constructor import core
+
+				# Create and configure a planner instance
 				pipelinePlanner = core.PipelinePlanner()
 				pipelinePlanner.planner = 'PRMkConfigDefault'
 				pipelinePlanner.num_planning_attempts = 10
-
-				# specify planning group
-				group = "panda_arm"
-
-				# create a task
-				task = core.Task()
-
-				# get the current robot state
-				currentState = stages.CurrentState("current state")
-				task.add(currentState)
-
-				# moveTo named posture, using the pipeline planner interplation
-				move = stages.MoveTo("moveTo ready", pipelinePlanner)
-				move.group = group
-				move.setGoal("extended")
-				task.add(move)
-
-		)")
+			)")
 	    .property<std::string>("planner", "str: Planner ID")
 	    .property<uint>("num_planning_attempts", "int: Number of planning attempts")
-	    .property<moveit_msgs::WorkspaceParameters>("workspace_parameters", R"(
-				WorkspaceParameters_: Workspace_parameters for the planning algorithm
-
-			.. _WorkspaceParameters: https://docs.ros.org/en/melodic/api/moveit_msgs/html/msg/WorkspaceParameters.html
-		)")
+	    .property<moveit_msgs::WorkspaceParameters>(
+	        "workspace_parameters",
+	        ":moveit_msgs:`WorkspaceParameters`: Specifies workspace box to be used for Cartesian sampling")
 	    .property<double>("goal_joint_tolerance", "float: Tolerance for reaching joint goals")
 	    .property<double>("goal_position_tolerance", "float: Tolerance for reaching position goals")
 	    .property<double>("goal_orientation_tolerance", "float: Tolerance for reaching orientation goals")
@@ -99,45 +81,39 @@ void export_solvers(py::module& m) {
 	    .property<bool>("publish_planning_requests", "bool: Publish motion planning requests via a topic")
 	    .def(py::init<>());
 
-	properties::class_<JointInterpolationPlanner, PlannerInterface>(m, "JointInterpolationPlanner", R"(
-			Interpolate a trajectory between states in joint space.
-			Fails if direct joint space interpolation fails.
-
-			::
+	properties::class_<JointInterpolationPlanner, PlannerInterface>(
+	    m, "JointInterpolationPlanner",
+	    R"(Perform linear interpolation between joint space poses.
+			Fails on collision along the interpolation path. There is no obstacle avoidance. ::
 
 				from moveit.task_constructor import core
 
-				# Instantiate joint interpolation planner
+				# Instantiate joint-space interpolation planner
 				jointPlanner = core.JointInterpolationPlanner()
 				jointPlanner.max_step = 0.1
 		)")
-	    .property<double>("max_step",
-	                      " float: Prevent large movements by specifying the maximum step distance in joint space")
+	    .property<double>("max_step", "float: Limit any (single) joint change between two waypoints to this amount")
 	    .def(py::init<>());
 
 	properties::class_<CartesianPath, PlannerInterface>(m, "CartesianPath", R"(
-			The planner uses MoveIt's ``computeCartesianPath()`` to
-			generate a straigh-line path between two scenes.
-
-			::
+			Perform linear interpolation between Cartesian poses.
+		 	Fails on collision along the interpolation path. There is no obstacle avoidance. ::
 
 				from moveit.task_constructor import core
 
-				# Instantiate cartesian path planner
+				# Instantiate Cartesian-space interpolation planner
 				cartesianPlanner = core.CartesianPath()
 				cartesianPlanner.step_size = 0.01
 				cartesianPlanner.jump_threshold = 0.0  # effectively disable jump threshold.
 		)")
-	    .property<double>("step_size", R"(
-			float: Specify the path interpolation resolution by adjusting the
-				step size between consecutive waypoints with this parameter.
-		)")
-	    .property<double>("jump_threshold", R"(
-			float: Prevent jumps in inverse kinematic solutions by adjusting the
-				acceptable fraction of mean joint motion per step.
-		)")
-	    .property<double>("min_fraction",
-	                      "float: Lower threshold of minimum motion per step required for success in planning")
+	    .property<double>("step_size", "float: Limit the Cartesian displacement between consecutive waypoints "
+	                                   "In contrast to joint-space interpolation, the Cartesian planner can also "
+	                                   "succeed when only a fraction of the linear path was feasible.")
+	    .property<double>(
+	        "jump_threshold",
+	        "float: Limit joint displacement between consecutive waypoints, thus preventing jumps in joint space. "
+	        "This values specifies the fraction of mean acceptable joint motion per step.")
+	    .property<double>("min_fraction", "float: Fraction of overall distance required to succeed.")
 	    .def(py::init<>());
 }
 }  // namespace python
