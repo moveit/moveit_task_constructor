4106e9000c4b691d37d3addb457f5d2c8a044913
Mario Prats <marioprats@gmail.com>
2023-03-21 23:10:23 -0700

    remove extra semicolon that triggers compiler warning

--- core/include/moveit/task_constructor/container.h
+++ core/include/moveit/task_constructor/container.h
@@ -163,7 +163,7 @@ class Fallbacks : public ParallelContainerBase
 	inline void replaceImpl();
 
 public:
-	PRIVATE_CLASS(Fallbacks);
+	PRIVATE_CLASS(Fallbacks)
 	Fallbacks(const std::string& name = "fallbacks");
 
 	void reset() override;
