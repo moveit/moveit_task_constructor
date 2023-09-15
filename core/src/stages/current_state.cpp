2b65b6fb8fdb3e63224026cd0e8a2dd009fd8124
Mario Prats <mario.prats@picknik.ai>
2023-09-15 15:01:01 +0200

    set a non-infinite default timeout in CurrentState stage

--- core/src/stages/current_state.cpp
+++ core/src/stages/current_state.cpp
@@ -35,6 +35,8 @@
 
 /* Authors: Michael Goerner, Luca Lach, Robert Haschke */
 
+#include <chrono>
+
 #include <moveit/task_constructor/stages/current_state.h>
 #include <moveit/task_constructor/storage.h>
 #include <moveit_msgs/srv/get_planning_scene.hpp>
@@ -47,13 +49,16 @@ namespace moveit {
 namespace task_constructor {
 namespace stages {
 
+using namespace std::chrono_literals;
+constexpr std::chrono::duration<double> DEFAULT_TIMEOUT = 3s;
+
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("CurrentState");
 
 CurrentState::CurrentState(const std::string& name) : Generator(name) {
 	auto& p = properties();
 	Property& timeout = p.property("timeout");
 	timeout.setDescription("max time to wait for get_planning_scene service");
-	timeout.setValue(-1.0);
+	timeout.setValue(DEFAULT_TIMEOUT.count());
 }
 
 void CurrentState::init(const moveit::core::RobotModelConstPtr& robot_model) {
