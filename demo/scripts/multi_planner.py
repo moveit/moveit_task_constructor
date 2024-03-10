5fd3ae6ff23069561829e45312c05100c2d178d0
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2024-03-10 18:31:19 +0100

    Revert "Enable parallel planning with PipelinePlanner (#450)"
    This reverts commit 0e02fcae77c8162b954675685ccdc7307f6f82e7.

--- demo/scripts/multi_planner.py
+++ demo/scripts/multi_planner.py
@@ -8,8 +8,10 @@ import time
 rclcpp.init()
 node = rclcpp.Node("mtc_tutorial")
 
-ompl_planner = core.PipelinePlanner(node, "ompl", "RRTConnectkConfigDefault")
-pilz_planner = core.PipelinePlanner(node, "pilz_industrial_motion_planner", "PTP")
+ompl_planner = core.PipelinePlanner(node, "ompl")
+ompl_planner.planner = "RRTConnectkConfigDefault"
+pilz_planner = core.PipelinePlanner(node, "pilz_industrial_motion_planner")
+pilz_planner.planner = "PTP"
 multiPlanner = core.MultiPlanner()
 multiPlanner.add(pilz_planner, ompl_planner)
 
