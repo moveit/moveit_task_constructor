cb867aeddfdca6e3791a4ede998fe23ce9958754
Karthik Arumugham <karthi.aru@gmail.com>
2024-08-27 14:29:30 +0530

    Fix run.launch.py (#607)
    Add path to panda_config.yaml file to have parameters available for pick_place_demo
    
    Co-authored-by: Robert Haschke <rhaschke@techfak.uni-bielefeld.de>

--- demo/launch/run.launch.py
+++ demo/launch/run.launch.py
@@ -1,3 +1,5 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
@@ -12,8 +14,10 @@ def generate_launch_description():
         .to_moveit_configs()
     )
 
+    package = "moveit_task_constructor_demo"
+    package_shared_path = get_package_share_directory(package)
     node = Node(
-        package="moveit_task_constructor_demo",
+        package=package,
         executable=LaunchConfiguration("exe"),
         output="screen",
         parameters=[
@@ -22,6 +26,7 @@ def generate_launch_description():
             moveit_config.robot_description_kinematics,
             moveit_config.joint_limits,
             moveit_config.planning_pipelines,
+            os.path.join(package_shared_path, "config", "panda_config.yaml"),
         ],
     )
 
