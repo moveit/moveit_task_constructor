d8a2c3b1cd7acf7f9411724eb3c785d2b66a7f52
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2024-08-27 10:01:12 +0200

    Add missing imports

--- demo/launch/run.launch.py
+++ demo/launch/run.launch.py
@@ -1,3 +1,5 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
