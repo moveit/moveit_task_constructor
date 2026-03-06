de703b960528d10d2858dcfa20e6abdb59fe4934
Demonmasterlqx <183842220@qq.com>
2026-03-06 23:05:46 +0800

    Update core/src/stage.cpp
    Co-authored-by: Robert Haschke <rhaschke@users.noreply.github.com>

--- core/src/stage.cpp
+++ core/src/stage.cpp
@@ -85,7 +85,7 @@ void InitStageException::append(InitStageException& other) {
 }
 
 const char* InitStageException::what() const noexcept {
-	static const char* msg = "Error initializing stage(s). ROS_ERROR_STREAM(e) for details. The error type is moveit::task_constructor::InitStageException.";
+	static const char* msg = "mtc::InitStageException. Use ROS_ERROR_STREAM(exc) for details.";
 	return msg;
 }
 
